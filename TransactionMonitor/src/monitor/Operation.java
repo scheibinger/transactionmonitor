acaaf9a5cdfd75aea2cf43f7d9512c9d51a0eaf1
jakub.mytnik <jakub.mytnik@3249c5d2-534f-11de-bb24-2d5e9b4af41d>
2009-06-21 12:08:42 +0000

    state saving

--- TransactionMonitor/src/monitor/Operation.java
+++ TransactionMonitor/src/monitor/Operation.java
@@ -4,8 +4,11 @@
  */
 package monitor;
 
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Vector;
 import monitor.queryobject.QueryObject;
 
 /**
@@ -46,8 +49,64 @@ public class Operation {
         this.success = success;
     }
 
-    public boolean execute(Statement stmt) throws SQLException {
+    public boolean executeXA(Statement stmt) throws SQLException {
         stmt.execute(this.query.getQuery());
+        return this.success;
+    }
+    public boolean execute(Statement stmt ) throws SQLException {
+        String queryType = this.query.getQueryType();
+        String queryContent = this.query.getQuery();
+        //lockowanie tabeli
+        try {
+        stmt.executeUpdate("LOCK TABLES "+query.getTableName()+" WRITE");
+        if (queryType.equals(QueryObject.INSERT)){
+
+            stmt.execute(queryContent,Statement.RETURN_GENERATED_KEYS);
+
+            //pobranie primary keys wstawionych rekordow
+            Vector keysVector = new Vector();
+            ResultSet keys  = stmt.getGeneratedKeys();
+            if (keys != null){
+                while (keys.next()){
+                    keysVector.add(keys.getString(1));
+                }
+            }
+            //pobranie nazwy kolumny ktora jest primary key
+            DatabaseMetaData dbmb = stmt.getConnection().getMetaData();
+            ResultSet primKey = dbmb.getPrimaryKeys(null, null, query.getTableName());
+            primKey.first();
+            String primKeyColName = primKey.getString("COLUMN_NAME");
+
+            //generowanie zapytania kompensujacego
+            query.generateInsertRollbackQuery(primKeyColName, keysVector);
+        } else {
+            //tworzenie tabeli tymczasowej z usuwanymi,zmienianymi rekordami - START
+            QueryObject selectQuery = new QueryObject(QueryObject.SELECT,query.getTableName(),
+                                                      query.getCriteria(),null);
+            //sprawdzenie czy sa rekordy spelniajace podane kryteria
+            stmt.execute(selectQuery.getQuery());
+            ResultSet srs = stmt.getResultSet();
+            if (srs != null){
+                stmt.execute("CREATE TABLE "+query.getTableName()+"_temp AS "+selectQuery.getQuery());
+            }
+            //tworzenie tabeli tymczasowej - KONIEC
+
+            //wykonanie wlasciwego zapytania
+            stmt.execute(query.getQuery());
+
+            //generowanie zapytania kompensujacego
+            if (queryType.equals(QueryObject.DELETE)){
+                query.generateDeleteRollbackQuery();
+            } else if (queryType.equals(QueryObject.UPDATE)){
+                query.generateUpdateRollbackQuery();
+            }
+
+
+        }
+        } catch(SQLException sqle){
+            this.success = false;
+            throw sqle;
+        }
         return this.success;
     }
 }
