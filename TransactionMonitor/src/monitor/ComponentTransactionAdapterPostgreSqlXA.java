b4870ea93de7d0a17298326909eafe8c0cb8a187
bielak.marcin <bielak.marcin@3249c5d2-534f-11de-bb24-2d5e9b4af41d>
2009-06-21 10:50:32 +0000

    Postgre XA

---
+++ TransactionMonitor/src/monitor/ComponentTransactionAdapterPostgreSqlXA.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package monitor;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ComponentTransactionAdapterPostgreSqlXA implements TransactionParticipantIF {
+
+
+    Connection connection;
+    DBConnectionData dbcd;
+    String key;
+    boolean success = true;
+
+    public boolean startTransaction(DBConnectionData dbcd, ComponentTransaction ct) {
+        this.key = String.valueOf(this.hashCode());
+        this.success = true;
+        this.dbcd = dbcd;
+        try {
+
+            Class.forName(this.dbcd.getDriver());
+            this.connection = DriverManager.getConnection(this.dbcd.url, this.dbcd.user, this.dbcd.password);
+        } catch (Exception ex) {
+            Logger.getLogger(ComponentTransactionAdapterMySqlXA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+        Statement stmt;
+        try {
+            stmt = connection.createStatement();
+
+            stmt.execute("BEGIN");
+
+            for (Operation oper : ct) {
+                oper.execute(stmt);
+            }
+            stmt.execute("PREPARE TRANSACTION '" + this.key + "'");
+        } catch (SQLException ex) {
+            this.success = false;
+            Logger.getLogger(ComponentTransactionAdapterMySqlXA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return this.success;
+    }
+
+
+    public boolean commitTransaction() {
+        try {
+            return connection.createStatement().execute("COMMIT PREPARED '" + this.key + "'");
+        } catch (SQLException ex) {
+            Logger.getLogger(ComponentTransactionAdapterMySqlXA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    public boolean abortTransaction() {
+        try {
+            return connection.createStatement().execute("ROLLBACK PREPARED '" + this.key + "'");
+        } catch (SQLException ex) {
+            Logger.getLogger(ComponentTransactionAdapterMySqlXA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+
+}
